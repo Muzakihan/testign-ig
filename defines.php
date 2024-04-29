3f291fece0fb65eb54d336e095af67d58f43478b
Muzakie <muzakifarha4@gmail.com>
2024-04-30 00:04:37 +0700

    ee

---
+++ defines.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+
+	define( 'FACEBOOK_APP_ID', '3727257384195754' );
+	define( 'FACEBOOK_APP_SECRET', '6e288d84872d9c92fc57084c8c16ac46' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://socialsizzle.heroku.com/auth' );
+	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
+
+	// accessToken
+	$accessToken = 'IGQWRQRnJtdzVVT2pjZAFFBS2lDbjJCQjgtTFl0eUh3UVZApY2pHenlad25PcFVEZAk1JcUx5ZADFEODBkMXl6VTRWa1dwLWdsLW8xcU1qOVUtMzQ0bXdKX2djMGFXOFZARR0FLQ2pERkRSZAEdmdwZDZD';
+
+	// page id
+	$pageId = 'YOUR-PAGE-ID';
+
+	// instagram business account id
+	$instagramAccountId = '58763120189';
